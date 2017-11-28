724a09577be669cbf5ba5840cdcbe55ddc385ee8
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-11-28 17:18:43 -0200

    Aula 02 - Controllers

---
+++ MVCMovieCore2/MVCMovieCore2/Controllers/HelloWorld.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVCMovieCore2.Controllers
+{
+    public class HelloWorld : Controller
+    {
+        // GET: /<controller>/
+        public string Index()
+        {
+            
+            return "Esta é minha visão padrão"; 
+        }
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+
+            ;
+        }
+    }
+}
